84e195ba286bb0f6733a322d1c2e941c50413a2c
chamoysans <157010512+chamoysans@users.noreply.github.com>
2025-04-11 14:05:47 +0800

    Removed the unused seeds thing

--- jokers/common/zathrax.lua
+++ jokers/common/zathrax.lua
@@ -17,7 +17,7 @@ local jokerThing = SMODS.Joker{
       }
     }, 
     rarity = 1, 
-    cost = 2, 
+    cost = 7, 
     order = 14,
     unlocked = true, 
     discovered = true, 
@@ -34,53 +34,24 @@ local jokerThing = SMODS.Joker{
       if context.selling_self and not context.retrigger_joker and not context.blueprint_card then
         ease_hands_played(3, false)
 
-        local seeds = {
-          'oh',
-          'wwo',
-          'wee',
-          'woo',
-          'wasdawsd',
-          'dsawdsaw',
-          'sk',
-          'ib',
-          'id',
-          'ir',
-          'iz',
-          'zl',
-          'er',
-          'in',
-          'osp',
-          'eaken',
-          'glish',
-          'helloworld',
-          'goodbyeworld',
-          'qwertyuiop',
-          'aqsdfghjkl',
-          'zxcvbnm',
-          'qazwsxedcrfv',
-          'tgbyhnujmikol',
-          'p'
-        }
-
         delay(0.3)
 
         G.FUNCS.draw_from_hand_to_discard()
 		    G.FUNCS.draw_from_discard_to_deck()
-
-        local seed = pseudorandom_element(seeds, pseudoseed('zathrax')) .. pseudorandom_element(seeds, pseudoseed('zathrax')) .. pseudorandom_element(seeds, pseudoseed('zathrax')) .. pseudorandom_element(seeds, pseudoseed('zathrax')) .. pseudorandom_element(seeds, pseudoseed('zathrax')) .. pseudorandom_element(seeds, pseudoseed('zathrax')) .. pseudorandom_element(seeds, pseudoseed('zathrax'))
-
-        sendTraceMessage("Seed: " .. seed .. ", Starting Shuffle", "ZathraxDebug")
-
+        
         for k, v in pairs(G.playing_cards) do
           v.ability.wheel_flipped = nil
         end
         G.E_MANAGER:add_event(Event({
           trigger = "immediate",
           func = function()
+            local oldState = G.STATE
             G.STATE = G.STATES.DRAW_TO_HAND
-            G.deck:shuffle("cry_reboot" .. G.GAME.round_resets.ante)
+            G.deck:shuffle("everydayamshuffling" .. G.GAME.round_resets.ante)
             G.deck:hard_set_T()
             G.STATE_COMPLETE = false
+            G.STATE = oldState
+            
             return true
           end,
         }))
